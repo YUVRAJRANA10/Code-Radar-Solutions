f9ba7025e6d42b0a820316c3cbdf5011b39ee024
Yuvraj singh <yuvraj.r0810@gmail.com>
2025-02-08 14:05:13 +0530

    Solution to Check Triangle Validity

---
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<math.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    // printf("%s", welcome());
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(pow(a,2)+ pow(b,2) = pow(c,2)){
+        printf("Valid");
+    }
+    else{
+        printf("Invalid");
+    }
+    return 0;
+}
